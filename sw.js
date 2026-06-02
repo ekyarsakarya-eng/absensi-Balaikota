831b8d386708e880b56d3f9c953f492228b11674
ekyarsakarya-eng <eky.arsakarya@gmail.com>
2026-06-02 08:07:20 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,28 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('fetch', e => {});
+const CACHE_NAME = 'absensi-blkt-v1';
+const urlsToCache = [
+  '/absensi-Balaikota/',
+  '/absensi-Balaikota/index.html',
+  '/absensi-Balaikota/app.js',
+  '/absensi-Balaikota/icon-192.png',
+  '/absensi-Balaikota/icon-512.png',
+  '/absensi-Balaikota/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
